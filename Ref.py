bdeba2599067fcde9f319ebef7bfcdd672b2ba86
ishige <hiroyukiishige@hotmail.com>
2023-05-13 07:41:56 +0700

    update

---
+++ Ref.py
@@ -0,0 +1,8 @@
+"""
+Reference site
+How to set scroll bar
+https://www.tutorialspoint.com/python/tk_scrollbar.htm
+
+
+
+"""
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 from tkinter import filedialog, messagebox
 from PIL import Image, ImageTk, ImageDraw, ImageFont
 
@@ -25,31 +26,48 @@ HEIGHT = 400 * 2
 # Set main window --------------------------------------------------------#
 root = Tk()
 root.title("Typing speed challenge")
-# root.minsize(width=WIDTH, height=HEIGHT)
-# root.config(padx=20, pady=20)  # padding for window
-# root.grid_rowconfigure(0, weight=1)
-# root.columnconfigure(0,weight=1)
+root.minsize(width=WIDTH, height=HEIGHT)
+root.config(padx=20, pady=20)  # padding for window
 
-frame_main = tkinter.Frame(root, bg="gray")
-frame_main.grid(column=0, row=3, sticky='news')
+# create frame_1 --------------------------
+frame_1 = tkinter.Frame(root, )
+frame_1.grid(column=0, row=0, sticky='news')
 
-# Set parts -------------------------------------------------------------#
+# Set canvas_1 on frame_1
+canvas_1_1 = tkinter.Canvas(frame_1, )
+canvas_1_1.grid(row=0, column=0, )
+# -----------------------------------------
+
+# create frame_2 --------------------------
+frame_2 = tkinter.Frame(root, bg="skyblue")
+frame_2.grid(column=0, row=1, sticky='news')
+
+# Set canvas_1 on frame_2
+canvas_1_2 = tkinter.Canvas(frame_2, )
+canvas_1_2.grid(row=0, column=0, )
+
+# Add a canvas_2 on frame_2
+canvas_2_2 = tkinter.Canvas(frame_2, )
+canvas_2_2.grid(row=1, column=0, )
+# ----------------------------------------
+
+# Set parts -----------------------------
 # Input name
-label_name = Label(text="Fill in your name", font=("Arial", 10))
+label_name = Label(canvas_1_1, text="Fill in your name", font=("Arial", 20))
 label_name.grid(column=0, row=0)
 label_name.config(padx=20, pady=20)
 
-input_name = Entry(width=20)
+input_name = Entry(canvas_1_1, width=30, font=("Arial", 20))
 input_name.grid(column=1, row=0)
 input_name.insert(tkinter.END, "Your name")
 
 # Show time
-label_time = Label(text="Time", font=("Arial", 30))
+label_time = Label(canvas_1_1, text="Time", font=("Arial", 20))
 label_time.grid(column=0, row=1)
 label_time.config(padx=20, pady=20)
 
 # Buttun to start
-button_start = Button(text="Start",
+button_start = Button(canvas_1_1, text="Start",
                       font=("Arial", 20),
                       command=start_countdown,
                       )
@@ -58,29 +76,29 @@ button_start.grid(column=1, row=1)
 # Get word for typing test. This is to get from Wikipedia
 text_to_type = get_wiki("Japan airlines")
 
-# create a frame for canvas------------------------------------------------------#
-frame_canvas = tkinter.Frame(frame_main)
-frame_canvas.grid(row=0, column=0, pady=(5, 0), sticky='nw')
-# frame_canvas.grid_rowconfigure(0, weight=1)
-# frame_canvas.grid_columnconfigure(0, weight=1)
-
-# Add a canvas in the frame
-canvas = tkinter.Canvas(frame_canvas, bg="white")
-canvas.grid(row=0, column=0,)
-
-# TODO add text box on Canvas
-text_box = Text(canvas, font=("arial", 20), height=10, width=60, )
+# add text box on Canvas 1 on frame 2
+text_box = Text(canvas_1_2, font=("arial", 20),
+                height=5,
+                # width=60,
+                )
 text_box.insert("1.0", text_to_type)
 text_box.grid(column=0, row=0)
 
-# Link scrollbar to the Canvas
-vsb = Scrollbar(canvas, orient="vertical", command=canvas.yview())
+# Link scrollbar to Canvas 1 on frame 2
+vsb = Scrollbar(canvas_1_2, orient="vertical", command=canvas_1_2.yview())
 vsb.grid(column=1, row=0, sticky="NS")
-text_box.configure(yscrollcommand=vsb.set)
-vsb.config(command=text_box.yview())
+vsb.config(command=text_box.yview)
+text_box.config(yscrollcommand=vsb.set)
 
-# Set the canvas scrolling region
-# canvas.config(scrollregion=canvas.bbox("all"))
+# Entry box to type on Canvas 2 on frame 2
+label_typeinput = Label(canvas_2_2, text="Type text here", font=("Arial", 20))
+label_typeinput.grid(column=0, row=0)
+label_typeinput.config(padx=20, pady=20)
+
+textfield = ScrolledText(canvas_2_2, wrap=tkinter.WORD, font=("arial", 20),
+                         height=5,
+                         )
+textfield.grid(column=0, row=1)
 
 # TODO set timer and forced to stop type
 # TODO count typed words
